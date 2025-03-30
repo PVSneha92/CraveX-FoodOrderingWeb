4bb5f281f0381281469d4f02285f7ca3e0a6c59b
sneha <pv.sneha999@gmail.com>
2025-03-30 16:32:30 +0530

    frontend and server

--- routes/couponRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { createCoupon } from '../controllers/couponController.js'
-import {userMiddleware,roleMiddleware } from '../middlewares/userMiddleware.js'
-
-const router = express.Router()
-
-router.post("/create",userMiddleware,roleMiddleware("Admin"),createCoupon)
-
-export const couponRouter = router
