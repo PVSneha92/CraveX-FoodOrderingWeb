60c34677d09ebca696b4ba5591c0e98ed62a26e1
sneha <pv.sneha999@gmail.com>
2025-04-15 02:11:50 +0530

    changed

--- admin/src/routes/ProtectRoutes.jsx
+++ admin/src/routes/ProtectRoutes.jsx
@@ -1,18 +1,18 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Outlet, useNavigate } from "react-router-dom";
 
 const ProtectRoutes = () => {
-  const navigate = useNavigate();
   const { isUserAuth } = useSelector((state) => state.user);
+  const navigate = useNavigate();
 
-  // // useEffect(() => {
-  // //   if (!isUserAuth) {
-  // //     navigate("/");
-  // //   }
-  // }, [isUserAuth, navigate]);
+  useEffect(() => {
+    if (!isUserAuth) {
+      navigate("/login");
+    }
+  }, [isUserAuth, navigate]);
 
-  return isUserAuth ? <Outlet /> : null;
+  return isUserAuth ? <Outlet /> : null; // return null to avoid flashing
 };
 
 export default ProtectRoutes;
