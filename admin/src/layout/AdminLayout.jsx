a26a698d0ba279aa9f2be92756f2c5c136cf819e
sneha <pv.sneha999@gmail.com>
2025-04-15 01:22:54 +0530

    adminlayout updated

--- admin/src/layout/AdminLayout.jsx
+++ admin/src/layout/AdminLayout.jsx
@@ -5,22 +5,21 @@ import { useDispatch, useSelector } from "react-redux";
 import { clearUserdata, saveUserData } from "../redux/features/userSlice";
 
 const AdminLayout = () => {
-  const { isUserAuth, userData } = useSelector((state) => state.user);
+  const { isUserAuth } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const location = useLocation();
+  const [loading, setLoading] = useState(true);
 
   const checkUser = async () => {
     try {
-      // Check if current path is under "/admin" or not
       const isAdminRoute = location.pathname.startsWith("/admin");
       const url = isAdminRoute ? "/check/admin" : "/check/restaurant";
-
       const response = await axiosInstance.get(url);
-      console.log(response, "response=======");
       dispatch(saveUserData(response?.data));
     } catch (error) {
-      console.error(error);
       dispatch(clearUserdata());
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -28,14 +27,16 @@ const AdminLayout = () => {
     checkUser();
   }, [location.pathname]);
 
+  if (loading) return <div>Loading...</div>;
+
   return (
     <div>
-      {/* Admin layout only renders the Outlet for nested routes */}
       <main>
         <Outlet />
       </main>
     </div>
   );
 };
+
 
 export default AdminLayout;
